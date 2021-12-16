5a8de6401cfcbf3f748e697e56c29375604e36e1
NeZorinEgor <92092053+NeZorinEgor@users.noreply.github.com>
2021-12-16 15:19:44 +0800

    Add files via upload

---
+++ turtle_moving.py
@@ -0,0 +1,86 @@
+import turtle
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('red')
+boba.speed(50)
+boba.penup()
+boba.goto(0,200)
+boba.pendown()
+
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('red')
+boba.speed(50)
+boba.penup()
+boba.goto(-250,200)
+boba.pendown()
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('red')
+boba.speed(50)
+boba.penup()
+boba.goto(250,200)
+boba.pendown()
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('blue')
+boba.speed(50)
+boba.penup()
+boba.goto(250,-50)
+boba.pendown()
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('blue')
+boba.speed(50)
+boba.penup()
+boba.goto(0,-50)
+boba.pendown()
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+turtle.shape("turtle")
+boba=turtle.Turtle()
+boba.pensize(2)
+boba.color('blue')
+boba.speed(50)
+boba.penup()
+boba.goto(-250,-50)
+boba.pendown()
+for i in range(80):
+    boba.circle(60)
+    boba.right(5)
+
+
+
+
+turtle.exitonclick()
+
+
