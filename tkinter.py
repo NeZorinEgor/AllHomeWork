b5672d416c9019952bdb9b7895a7b2039bff14da
NeZorinEgor <92092053+NeZorinEgor@users.noreply.github.com>
2021-12-23 17:40:55 +0800

    Add files via upload

--- tkinter.py
+++ tkinter.py
@@ -1,23 +1,95 @@
+
 import tkinter as tk
+file = open("tinker.txt", "r")
+def inst():
+    t.pack()
+window = tk.Tk()
+window.title("Тестовая система")
+window.geometry("300x150")
+score = 0
+false=0
+allq=0
+close=0
+def start():
+        global line,out,proc,false,allq,close
+        line = file.readline()
+        line = line.rstrip()
+        close += 1
+        if close==1:
+            window.destroy()
+        else:
+            close+=1
+        if line=='':
+            allq=score+false
+            print(int((score/allq)*100))
+            win = tk.Tk()
+            win.title("Результаы ОГЭ")
+            win.geometry("640x480")
+            out = tk.Label(win, text='Правельный ответ '+ str(score)+' вопросов.',font='Calibri 14')
+            out.pack()
+            proc= tk.Label(win, text='Процент правильных ответов =  '+ str(int((score/allq)*100))+' %', font='Calibri 14')
+            proc.pack()
+            x = (win.winfo_screenwidth() - win.winfo_reqwidth()) / 2
+            y = (win.winfo_screenheight() - win.winfo_reqheight()) / 2
+            win.wm_geometry("+%d+%d" % (x, y))
+        else:
+
+            root = tk.Tk()
+            root.geometry('1024x150')
+            root.title('q.txt')
+            q = tk.Label(root, text=line)
+            q.pack()
+            line = file.readline()
+            line = line.rstrip()
+            a = tk.Label(root, text="1. "+line)
+            a.pack()
+            line = file.readline()
+            line = line.rstrip()
+            b = tk.Label(root, text="2. "+line)
+            b.pack()
+            line = file.readline()
+            line = line.rstrip()
+            c = tk.Label(root, text="3. "+line)
+            c.pack()
+            ans = tk.Entry(root, width=40)
+            x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
+            y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+            root.wm_geometry("+%d+%d" % (x, y))
+            ans.pack()
+            def submit():
+                global otvet,score,false
+                otvet = str(ans.get())
+                line = file.readline()
+                line = line.rstrip()
+                if otvet != line:
+                    print('Нет, ответ:',line)
+                    false += 1
+                    root.destroy()
+                    start()
+
+                else:
+                    print('Правильно')
+                    score += 1
+                    start()
+                    root.destroy()
+            sub = tk.Button(root, text="Принять", command=submit)
+            sub.pack()
 
 
-def func():
-    try:
-        x = float(entry_1.get())
-        y = float(entry_2.get())
-        label.config(text="Сумма числе будет равна {}".format(x + y))
-    except ValueError:
-        label.config(text="Ошибка введите цифры")
 
 
-root = tk.Tk()
-root.geometry("640x480")
-entry_1 = tk.Entry(root)
-entry_2 = tk.Entry(root)
-entry_1.pack()
-entry_2.pack()
-label = tk.Label(root, text="Сумма числе будет равна")
-label.pack()
-button = tk.Button(root, text='сложить числа', command=func)
-button.pack()
-root.mainloop()
+
+greet = tk.Label(window, text="ОГЭ по Истории" ,font='Calibri 14')
+greet.pack()
+startButton = tk.Button(window, command=start, text="Начать тест")
+startButton.pack()
+
+
+end = tk.Button(window, text="Завершить", command=window.destroy)
+end.pack()
+
+
+x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2
+y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2
+window.wm_geometry("+%d+%d" % (x, y))
+window.mainloop()
