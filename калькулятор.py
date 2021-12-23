cf6f5a962f094b7b787b0069511ca7f43d6ef00e
NeZorinEgor <92092053+NeZorinEgor@users.noreply.github.com>
2021-12-23 15:59:26 +0800

    Add files via upload

--- калькулятор.py
+++ калькулятор.py
@@ -1,15 +1,26 @@
-n = input("Операция( + - * /): ")
-a = float(input('a = '))
-b = float(input('b = '))
-e = "Оба значения должны быть числами!"
-if n == '+':
-    print(a + b)
-elif n == '-':
-    print(a - b)
-elif n == '*':
-    print(a * b)
-elif n == '/':
-    if b != 0:
-        print(a / b)
+while True:
+    s = input("Введите оператор (+, -, *, / или stop (стоп) для остановки программы): ")
+    if s == 'stop' or s == 'стоп':
+        print("Программа остановлена")
+        break
+    if s in ('+', '-', '*', '/'):
+        while True:
+            try:
+                n1 = int(input("Введите первое число = "))
+                n2 = int(input("Введите второе число = "))
+                break
+            except ValueError:
+                print("Программа работает только с числами!")
+        if s == '+':
+            print("Oтвет:",('{} + {} ='.format(n1, n2)),(n1+n2))
+        elif s == '-':
+            print("Oтвет:",('{} - {} ='.format(n1, n2)),(n1-n2))
+        elif s == '*':
+            print("Oтвет:",('{} * {} ='.format(n1, n2)),(n1*n2))
+        elif s == '/':
+            if n2 != 0:
+                print("Oтвет:",('{} / {} ='.format(n1, n2)),(n1/n2))
+            else:
+                print("Деление на ноль нельзя!")
     else:
-        print("Делить на ноль нельзя!")
+        print("Вы ввели неправильный оператор! Повторите попытку.")
